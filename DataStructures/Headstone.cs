1c98f2eb19b81b6164ff9a96819a15ff45f32cdb
Samuel Whitehead <Samuel.Whitehead@mines.sdsmt.edu>
2019-11-24 13:34:51 -0700

    Added headston and person class

---
+++ DataStructures/Headstone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Headstone
+    {
+        string CemeteryName;
+        string BurialSectionNumber;
+        string WallID;
+        string RowNum;
+        string GavestoneNumber;
+        string MarkerType;
+        string Emblem1;
+        string Emblem2;
+
+        Person PrimaryDecedent;
+        List<Person> OthersDecedentList;
+
+        string Image1FilePath;
+        string Image2FilePath;
+    }
+}
---
+++ DataStructures/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Person
+    {
+        string FirstName;
+        string MiddleName;
+        string LastName;
+        string Suffix;
+        string Location;
+        List<string> RankList;
+        List<string> AwardList;
+        string AwardCustom;
+        List<string> WarList;
+        List<string> BranchList;
+        string BranchUnitCustom;
+        DateTime BirthDate;
+        DateTime DeathDate;
+        string Inscription;
+    }
+}
