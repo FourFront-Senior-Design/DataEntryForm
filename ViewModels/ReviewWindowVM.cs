a1092fd7cd75b77c5f0530c64f2ebb95a67b4643
Morgan Benninghofen <morgan.benninghofen@mines.sdsmt.edu>
2019-12-03 13:59:04 -0700

    Added insert to database, needs testing

--- ViewModels/ReviewWindowVM.cs
+++ ViewModels/ReviewWindowVM.cs
@@ -39,6 +39,10 @@ namespace ViewModels
             }
             set
             {
+                if (_currentPageIndex != 0)
+                {
+                    _database.SetHeadstone(_currentPageIndex, _currentPageData);
+                }
                 _currentPageIndex = value;
                 displayHeadStone();
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PageIndex)));
