3dc3912ee9de42523879af78c561f7cad5246913
Morgan Benninghofen <morgan.benninghofen@mines.sdsmt.edu>
2019-12-07 13:03:06 -0700

    Changed build target to x64

--- Services/MicrosoftAccess.cs
+++ Services/MicrosoftAccess.cs
@@ -59,6 +59,7 @@ namespace Services
                     }
                     catch (Exception e)
                     {
+                        Console.WriteLine(e);
                         return false;
                     }
                 }
